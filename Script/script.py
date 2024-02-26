603fb71f6579e83f005e8626bfd2b2f8485cee71
simonbarrios <sbarrios333@gmail.com>
2024-02-26 09:34:20 -0400

    Added logic to the menu

--- Script/script.py
+++ Script/script.py
@@ -67,49 +67,55 @@ print("Made by Simon Barrios")
 print("\n\nWelcome to Blackjack!")
 player_name = input("Please enter your name: ")
 
-print("""\n\nGame menu:
-      1 - Play Game
-      2 - Game rules
-      3 - Exit""")
+while True:
+    print("""\n\nGame menu:
+        1 - Play Game
+        2 - Exit""")
 
-choice = input("Choice: ")
+    choice = input("Choice: ")
 
-if int(choice) == 1:
-    player_hand = []
-    dealer_hand = []
+    if int(choice) == 1:
+        player_hand = []
+        dealer_hand = []
 
-    deal_card(deck, player_hand)
-    deal_card(deck, player_hand)
-    deal_card(deck, dealer_hand)
-    deal_card(deck, dealer_hand)
+        deal_card(deck, player_hand)
+        deal_card(deck, player_hand)
+        deal_card(deck, dealer_hand)
+        deal_card(deck, dealer_hand)
 
-    while True:
-        print(f"{player_name}'s hand = {player_hand} ({calculate_hand_value(player_hand)})")
-        print(f"Dealers hand = [{deal_card[0]}, <face down>]")
+        while True:
+            print(f"{player_name}'s hand = {player_hand} ({calculate_hand_value(player_hand)})")
+            print(f"Dealers hand = [{dealer_hand[0]}, <face down>]")
+
+            if calculate_hand_value(player_hand) > 21:
+                print("Player bust!")
+                break
+
+            action = input("Do you want hit or stand?: ")
+
+            if action.lower() == "hit":
+                deal_card(deck, player_hand)
+            else:
+                break
+        
+        print(f"{player_name}'s Hand = {player_hand} ({calculate_hand_value(player_hand)})")
+        print(f"Dealer Hand = {dealer_hand} ({calculate_hand_value(dealer_hand)})")
 
         if calculate_hand_value(player_hand) > 21:
             print("Player bust!")
-            break
-
-        action = input("Do you want hit or stand?: ")
-
-        if action.lower() == "hit":
-            deal_card(deck, player_hand)
+        elif calculate_hand_value(dealer_hand) > 21:
+            print(f"Dealer bust! {player_name} wins!")
+        elif calculate_hand_value(player_hand) > calculate_hand_value(dealer_hand):
+            print("Player wins!")
+        elif calculate_hand_value(player_hand) < calculate_hand_value(dealer_hand):
+            print("Dealer wins!")
         else:
-            break
-    
-    print(f"{player_name}'s Hand = {player_hand} ({calculate_hand_value(player_hand)})")
-    print(f"Dealer Hand = {dealer_hand} ({calculate_hand_value(dealer_hand)})")
+            print("Push!")
 
-    if calculate_hand_value(player_hand) > 21:
-        print("Player bust!")
-    elif calculate_hand_value(dealer_hand) > 21:
-        print(f"Dealer bust! {player_name} wins!")
-    elif calculate_hand_value(player_hand) > calculate_hand_value(dealer_hand):
-        print("Player wins!")
-    elif calculate_hand_value(player_hand) < calculate_hand_value(dealer_hand):
-        print("Dealer wins!")
+    elif int(choice) == 2:
+        print("Thanks for playing!")
+        break
     else:
-        print("Push!")
+        print("Choose a valid option!")
 
 
